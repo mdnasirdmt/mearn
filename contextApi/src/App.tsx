b24537f377916433248b16751b93773182dc87ae
Md Nasir uddin <mdnasirdmt@gmail.com>
2023-10-25 15:43:31 +0530

    context api practice

---
+++ contextApi/src/App.tsx
@@ -0,0 +1,14 @@
+
+import "./App.css";
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-slate-400 text-center p-5">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
